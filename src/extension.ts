ce1a42dee9af8b089010bf0c10ed95a051c7b070
R. G. Esteves <rgesteves@gmail.com>
2018-10-06 16:32:07 -0700

    Loading local resources

--- src/extension.ts
+++ src/extension.ts
@@ -1,28 +1,22 @@
 'use strict';
 
 import * as vscode from 'vscode';
-
-const htmlContent : string =`
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="with=device-width, initial-scale=1.0">
-  </head>
-  <body>
-    <h1>Testing...(fixed metas in headers) </h1>
-  </body>
-</html>
-`;
+import * as path from 'path';
+import * as fs from 'fs';
 
 export function activate(context: vscode.ExtensionContext) {
+    let extensionPath = context.extensionPath;
+    let mediaPath = path.join(extensionPath, 'media');
 
-    console.log(`Extension "testd3" is now active, running from ${context.extensionPath}.`);
+    console.log(`Extension "testd3" is now active, running from ${extensionPath}.`);
+    console.log(`Media path ${fs.existsSync(mediaPath)?"":"not "}found.`);
+
+    let kittenPath = vscode.Uri.file(path.join(mediaPath, 'kitten.jpg')).with({ scheme : 'vscode-resource'});
 
     let currentPanel : vscode.WebviewPanel | undefined = undefined;
 
     let disposable = vscode.commands.registerCommand('extension.testD3js', () => {
-       currentPanel = createOrShowPanel(currentPanel);
+       currentPanel = createOrShowPanel(currentPanel, kittenPath);
     });
 
     context.subscriptions.push(disposable);
@@ -33,7 +27,7 @@ export function deactivate() {
     /* empty */
 }
 
-function createOrShowPanel(_panel : vscode.WebviewPanel | undefined ) 
+function createOrShowPanel(_panel : vscode.WebviewPanel | undefined, imgUri : vscode.Uri ) 
 : vscode.WebviewPanel
 {
     let panel : vscode.WebviewPanel;
@@ -48,9 +42,26 @@ function createOrShowPanel(_panel : vscode.WebviewPanel | undefined )
     }
 
     panel.title = "Testing Panel";
-    panel.webview.html = htmlContent;
+    panel.webview.html = getHtmlContent(imgUri);
 
     // Display a message box to the user
     vscode.window.showInformationMessage('[aspirational] displaying a d3-powered view!');
     return panel;
+}
+
+function getHtmlContent(imgUri : vscode.Uri) : string {
+    let htmlContent : string =`
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="with=device-width, initial-scale=1.0">
+  </head>
+  <body>
+    <h1>Testing...(fixed metas in headers) </h1>
+    <img src="${imgUri}" width="300" />
+  </body>
+</html>
+`;
+    return htmlContent;
 }
