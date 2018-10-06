b1b2831b9c5b8b16def4d75af9a0959e0d4f9ae9
R. G. Esteves <rgesteves@gmail.com>
2018-10-06 13:02:22 -0700

    Display a webpanel

--- src/extension.ts
+++ src/extension.ts
@@ -2,13 +2,25 @@
 
 import * as vscode from 'vscode';
 
+const htmlContent : string =`
+<!doctype html>
+<html>
+  <body>
+    <h1>Testing...</h1>
+  </body>
+</html>
+`;
+
 export function activate(context: vscode.ExtensionContext) {
 
-    console.log('Extension "testd3" is now active!');
+    console.log(`Extension "testd3" is now active, running from ${context.extensionPath}.`);
 
     let disposable = vscode.commands.registerCommand('extension.testD3js', () => {
+        /*
         // Display a message box to the user
         vscode.window.showInformationMessage('Should be displaying a d3-powered view!');
+        */
+       commandHandler();
     });
 
     context.subscriptions.push(disposable);
@@ -17,4 +29,16 @@ export function activate(context: vscode.ExtensionContext) {
 // this method is called when your extension is deactivated
 export function deactivate() {
     /* empty */
+}
+
+function commandHandler() {
+    // Open a webview
+    let panel = vscode.window.createWebviewPanel("testType",
+    "Panel display",
+    vscode.ViewColumn.Two
+    );
+    panel.title = "Testing Panel";
+    panel.webview.html = htmlContent;
+    // Display a message box to the user
+    vscode.window.showInformationMessage('[aspirational] displaying a d3-powered view!');
 }
