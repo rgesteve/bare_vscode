789bb33bbc03f8c121e865c2689585a822b0062f
R. G. Esteves <rgesteves@gmail.com>
2018-10-06 21:10:49 -0700

    Enabled communication with extension

--- src/extension.ts
+++ src/extension.ts
@@ -5,6 +5,7 @@ import * as path from 'path';
 import * as fs from 'fs';
 
 export function activate(context: vscode.ExtensionContext) {
+
     let extensionPath = context.extensionPath;
     let mediaPath = path.join(extensionPath, 'media');
 
@@ -15,8 +16,7 @@ export function activate(context: vscode.ExtensionContext) {
 
     let currentPanel : vscode.WebviewPanel | undefined = undefined;
 
-    let disposable = vscode.commands.registerCommand('extension.testD3js', () => {
-       //currentPanel = createOrShowPanel(currentPanel, kittenPath);
+    let createPanelDisposable = vscode.commands.registerCommand('extension.testD3js', () => {
        if (currentPanel) {
          currentPanel.reveal(vscode.ViewColumn.Two);
        } else {
@@ -28,12 +28,25 @@ export function activate(context: vscode.ExtensionContext) {
              undefined,
              context.subscriptions
          );
+         currentPanel.webview.onDidReceiveMessage(msg => {
+            vscode.window.showInformationMessage(`Seems like I got a message ${msg.command}!`);
+         }, undefined, context.subscriptions);
        }
        // Display a message box to the user
        vscode.window.showInformationMessage('[aspirational] displaying a d3-powered view!');
     });
 
-    context.subscriptions.push(disposable);
+    let talkPanelDisposable = vscode.commands.registerCommand("extension.talkD3js", () => {
+        if (!currentPanel) {
+            vscode.window.showInformationMessage('Need to have the webview open');
+        } else {
+            vscode.window.showInformationMessage('Sending a message to the webview');
+            currentPanel.webview.postMessage({ command : 'refactor'});
+        }
+    });
+
+    context.subscriptions.push(createPanelDisposable);
+    context.subscriptions.push(talkPanelDisposable);
 }
 
 // this method is called when your extension is deactivated
@@ -56,13 +69,34 @@ function getHtmlContent(imgUri : vscode.Uri) : string {
     <h2>Local lines counter</h2>
     <div id="lines-of-code-counter">0</div>
 
+    <h2>Latest communication from host</h2>
+    <div id="latest-comm"></div>
+
     <script>
+    (function () {
+        const vscode = acquireVsCodeApi();
         const counter = document.getElementById("lines-of-code-counter");
+        const commMsg = document.getElementById("latest-comm");
 
         let count = 0;
         setInterval( () => {
             counter.textContent = count++;
-        }, 100);
+            if ((count % 25) === 0) {
+                console.log("Sending a message to the host...");
+                vscode.postMessage({
+                    command: 'alert',
+                    text: 'communicating with host'
+                });
+            }
+        }, 500);
+
+        // Handle a message inside the webview
+        window.addEventListener('message', event => {
+            const message = event.data; // the message data the host sent
+            commMsg.textContent = "Just received a message at time [" + count + "]";
+            console.log("This should be displayed in the developer tools if they're open");
+        });
+    })();
     </script>
   </body>
 </html>
