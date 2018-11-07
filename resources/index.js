a7a4f9ec675cdfedc8f0f94dac62c465e45cccc2
R. G. Esteves <rgesteves@gmail.com>
2018-11-07 14:48:53 -0800

    Cleaned up some unused code

--- resources/index.js
+++ resources/index.js
@@ -2,6 +2,4 @@ window.$ = require('jquery');
 require('d3');
 c3 = require('c3');
 agGrid = require('ag-grid-community');
-//require('ag-grid-community');
-//import * as agGrid from 'ag-grid-community';
 //require('./main.js');
