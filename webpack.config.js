c57e31efc231cfb36e265d9541450372db4a3230
R. G. Esteves <rgesteves@gmail.com>
2018-11-07 08:31:33 -0800

    Support for webpack

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require ( 'path' );
+
+module.exports = {
+  target: 'node',
+  entry: './resources/index.js',
+  mode : 'development',
+  output: {
+    path: path.resolve ( __dirname, '/resources' ),
+    filename: 'bundle.js',
+    libraryTarget: 'commonjs2'
+  },
+  externals: {
+    vscode: 'commonjs vscode'
+  },
+  resolve: {
+    extensions: ['tsx', '.ts', '.jsx', '.js']
+  },
+  module: {
+    rules: [
+      { test: /\.ts$/, exclude: /node_modules/, use: [ { loader: 'ts-loader' } ] }
+    ]
+  }
+}
